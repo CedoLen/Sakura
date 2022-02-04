bc3f1a34ecea425216870178b5c2795b8b01142e
Cedox <ps-2004@bk.ru>
2022-02-04 11:25:08 +0500

    Validation Adding validation to input fields

--- app/src/main/java/com/example/sakura/SignInScreen.kt
+++ app/src/main/java/com/example/sakura/SignInScreen.kt
@@ -2,10 +2,52 @@ package com.example.sakura
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import java.util.regex.Pattern.compile
 
 class SignInScreen : AppCompatActivity() {
+    lateinit var email: EditText
+    lateinit var password:EditText
+    lateinit var pattern:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in_screen)
+
+        email = findViewById(R.id.editTextEmailAddress)
+        password= findViewById(R.id.editTextPassword)
+        pattern = "/^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}\$/i"
+    }
+
+    fun EmailValid(email:String):Boolean{ return compile(pattern).matcher(email).matches()}
+
+    fun goToSignIn(view: View) {
+        if(email.text.isNotEmpty() && password.text.isNotEmpty())
+        {
+            if(EmailValid(email.text.toString()))
+            {
+
+            }
+            else
+            {
+                var alertDialog = AlertDialog.Builder(this)
+                    .setTitle("Ошибка")
+                    .setMessage("Ошибка в поле \"Почта\".")
+                    .setPositiveButton("OK",null)
+                    .create()
+                    .show()
+            }
+        }
+        else
+        {
+            var alertDialog = AlertDialog.Builder(this)
+                .setTitle("Ошибка")
+                .setMessage("Введите логин и пароль.")
+                .setPositiveButton("OK",null)
+                .create()
+                .show()
+        }
     }
 }
